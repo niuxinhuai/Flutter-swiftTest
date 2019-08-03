11cd0c6036f79dad78180e06c2ee71cbfb88805b
牛新怀 <niuxinhuai@fanfandata.com>
2019-08-03 11:05:40 +0800

    add url

--- my_flutter/lib/Const/API.dart
+++ my_flutter/lib/Const/API.dart
@@ -1,7 +1,5 @@
-
 class API {
-
+  static const String base_url = 'https://www.wanandroid.com/';
   static const String posts = 'https://jsonplaceholder.typicode.com/posts';
-  static const String home_list = 'http://www.wanandroid.com/article/list/';
-
+  static const String home_list = "/banner/json";
 }
